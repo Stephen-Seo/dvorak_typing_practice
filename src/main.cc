a43f83d7d5e158c3b3a4e19061b676b045816d8a
Stephen Seo <seo.disparate@gmail.com>
2023-12-14 20:16:09 +0900

    Add CMakeLists.txt
    Fix warnings related to -Weffc++.
    
    Format with clang-format.

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,7 @@
 // Standard library includes
-#include <string>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <string>
 
 // Third party includes
 #include <raylib.h>
@@ -10,58 +10,59 @@
 #include "screen.h"
 
 std::string get_next_word(std::istream *i) {
-    std::string word;
-    while (i->good()) {
-        int in = i->get();
-        if (in != std::char_traits<char>::eof() && in != ' ' && in != '\n' && in != '\r') {
-            word.push_back((char)in);
-        } else if (in == ' '|| in == '\n' || in == '\r' || in == '\t') {
-            if (!word.empty()) {
-                break;
-            } else {
-                continue;
-            }
-        }
+  std::string word;
+  while (i->good()) {
+    int in = i->get();
+    if (in != std::char_traits<char>::eof() && in != ' ' && in != '\n' &&
+        in != '\r') {
+      word.push_back((char)in);
+    } else if (in == ' ' || in == '\n' || in == '\r' || in == '\t') {
+      if (!word.empty()) {
+        break;
+      } else {
+        continue;
+      }
     }
+  }
 
-    return word;
+  return word;
 }
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cout << "Expected 1 arg input filename!\n";
-        return 1;
-    }
+  if (argc != 2) {
+    std::cout << "Expected 1 arg input filename!\n";
+    return 1;
+  }
 
-    std::ifstream ifs(argv[1]);
+  std::ifstream ifs(argv[1]);
 
-    if (!ifs.good()) {
-        std::cout << "ERROR: Failed to open file \"" << argv[1] << "\"!\n";
-        return 2;
-    }
+  if (!ifs.good()) {
+    std::cout << "ERROR: Failed to open file \"" << argv[1] << "\"!\n";
+    return 2;
+  }
 
-    InitWindow(900, 400, "Dvorak Typing Practice");
-    SetTargetFPS(15);
+  InitWindow(900, 400, "Dvorak Typing Practice");
+  SetTargetFPS(15);
 
-    {
-        Screen screen{};
+  {
+    Screen screen{};
 
-        std::string word;
+    std::string word;
 
-        do {
-            word = get_next_word(&ifs);
-            if (!word.empty()) {
-                screen.set_word(word);
-                while (!screen.update() && !WindowShouldClose()) {
-                    BeginDrawing();
-                    ClearBackground(BLACK);
-                    screen.draw();
-                    EndDrawing();
-                }
-            }
-        } while (!ifs.eof() && !WindowShouldClose());
-    }
+    do {
+      word = get_next_word(&ifs);
+      if (!word.empty()) {
+        screen.set_word(word);
+        while (!screen.update() && !WindowShouldClose()) {
+          BeginDrawing();
+          ClearBackground(BLACK);
+          screen.draw();
+          EndDrawing();
+        }
+      }
+    } while (!ifs.eof() && !WindowShouldClose());
+  }
 
-    CloseWindow();
-    return 0;
+  CloseWindow();
+  return 0;
 }
--- src/screen.h
+++ src/screen.h
@@ -20,31 +20,30 @@ constexpr int KEY_HALF_SIZE = 29;
 constexpr Color KEY_HIGHLIGHT_COLOR = Color{255, 255, 0, 127};
 
 class Screen {
-public:
-    Screen();
-    ~Screen();
+ public:
+  Screen();
+  ~Screen();
 
-    void set_word(std::string s);
+  void set_word(std::string s);
 
-    // True if finished with word.
-    bool update();
-    void draw();
+  // True if finished with word.
+  bool update();
+  void draw();
 
-private:
-    std::string prev;
-    std::string word;
-    std::string word_pre;
-    std::string word_post;
-    std::array<char, 2> word_center;
-    Texture2D keyboard;
-    unsigned int idx;
-    /*
-     * 0 - cached word data is dirty
-     */
-    std::bitset<32> flags;
+ private:
+  std::string prev;
+  std::string word;
+  std::string word_pre;
+  std::string word_post;
+  std::array<char, 2> word_center;
+  Texture2D keyboard;
+  unsigned int idx;
+  /*
+   * 0 - cached word data is dirty
+   */
+  std::bitset<32> flags;
 
-    void draw_overlay(char c);
-
+  void draw_overlay(char c);
 };
 
 #endif
